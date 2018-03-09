5fdb5446bd54846aced5604f2164605f9ebf5788
mr. keith <keith@hoerling.com>
2018-03-09 00:31:56 -0800

    Revert "adds openMax prop"
    This reverts commit e15559635ef8597428e2ae6237ed7a14a11408d4.

--- src/Drawer.js
+++ src/Drawer.js
@@ -37,8 +37,6 @@ export default class Drawer extends Component {
     headerIcon: PropTypes.string,
     // Height of the visible teaser area at the bottom of the screen
     teaserHeight: PropTypes.number,
-    // Offset from top, in addition to headerHeight, that drawer should open
-    openMax: PropTypes.number,
   };
 
   // Set default prop values
@@ -48,7 +46,6 @@ export default class Drawer extends Component {
     header: 'Messages',
     headerHeight: 70,
     teaserHeight: 75,
-    openMax: 0,
     headerIcon: 'md-arrow-up',
   };
 
@@ -72,9 +69,9 @@ export default class Drawer extends Component {
       // starting value - teaserHeight higher than the bottom of the screen
       start: height - this.props.teaserHeight,
       // end value - headerHeight lower than the top of the screen
-      end: this.props.headerHeight + this.props.openMax,
+      end: this.props.headerHeight,
       // minimal possible value - a bit lower the top of the screen
-      min: this.props.headerHeight + (this.props.isLocked ? 250 : this.props.openMax),
+      min: this.props.headerHeight + (this.props.isLocked ? 250 : 25),
       // When animated triggers these value updates
       animates: [
         () => this._animatedOpacity,
